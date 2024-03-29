87cc16a9024259ca4e4c651ac5a9c0c343af1d5e
luis enrique <supelanosuarezluisenrique5@gmail.com>
2024-03-29 00:23:33 -0500

    curso termiando

--- Program.cs
+++ Program.cs
@@ -1,110 +1,105 @@
-﻿using System;
-using System.Collections.Generic;
+﻿List<string> TaskList = new List<string>();
 
-namespace ToDo
+int menuSelected = 0;
+do
 {
-    internal class Program
+    menuSelected = ShowMainMenu();
+    if ((menuOptions)menuSelected == menuOptions.Add)
     {
-        public static List<string> TL { get; set; }
+        ShowMenuAdd();
+    }
+    else if ((menuOptions)menuSelected == menuOptions.Remove)
+    {
+        ShowMenuRemove();
+    }
+    else if ((menuOptions)menuSelected == menuOptions.List)
+    {
+        ShowMenuTaskList();
+    }
+} while ((menuOptions)menuSelected != menuOptions.Exit);
 
-        static void Main(string[] args)
+
+int ShowMainMenu()
+{
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine("Ingrese la opción a realizar: ");
+    Console.WriteLine("1. Nueva tarea");
+    Console.WriteLine("2. Remover tarea");
+    Console.WriteLine("3. Tareas pendientes");
+    Console.WriteLine("4. Salir");
+
+    string userOptionMenu = Console.ReadLine();
+    return Convert.ToInt32(userOptionMenu);
+}
+
+void ShowMenuRemove()
+{
+    try
+    {
+        Console.WriteLine("Ingrese el número de la tarea a remover: ");
+        showTaskList();
+
+        string userOptionRemove = Console.ReadLine();
+        int indexTaskToRemove = Convert.ToInt32(userOptionRemove) - 1;
+        if (indexTaskToRemove <= (TaskList.Count - 1) && indexTaskToRemove >= 0)
         {
-            TL = new List<string>();
-            int variable = 0;
-            do
-            {
-                variable = ShowMainMenu();
-                if (variable == 1)
-                {
-                    ShowMenuAdd();
-                }
-                else if (variable == 2)
-                {
-                    ShowMenuDos();
-                }
-                else if (variable == 3)
-                {
-                    ShowMenuTres();
-                }
-            } while (variable != 4);
+            string deletedTask = TaskList[indexTaskToRemove];
+            TaskList.RemoveAt(indexTaskToRemove);
+            Console.WriteLine($"Tarea {deletedTask} eliminada");
         }
-        /// <summary>
-        /// Show the main menu 
-        /// </summary>
-        /// <returns>Returns option indicated by user</returns>
-        public static int ShowMainMenu()
+        else
         {
-            Console.WriteLine("----------------------------------------");
-            Console.WriteLine("Ingrese la opción a realizar: ");
-            Console.WriteLine("1. Nueva tarea");
-            Console.WriteLine("2. Remover tarea");
-            Console.WriteLine("3. Tareas pendientes");
-            Console.WriteLine("4. Salir");
-
-            // Read line
-            string line = Console.ReadLine();
-            return Convert.ToInt32(line);
-        }
-
-        public static void ShowMenuDos()
-        {
-            try
-            {
-                Console.WriteLine("Ingrese el número de la tarea a remover: ");
-                // Show current taks
-                for (int i = 0; i < TL.Count; i++)
-                {
-                    Console.WriteLine((i + 1) + ". " + TL[i]);
-                }
-                Console.WriteLine("----------------------------------------");
-
-                string line = Console.ReadLine();
-                // Remove one position
-                int indexToRemove = Convert.ToInt32(line) - 1;
-                if (indexToRemove > -1)
-                {
-                    if (TL.Count > 0)
-                    {
-                        string task = TL[indexToRemove];
-                        TL.RemoveAt(indexToRemove);
-                        Console.WriteLine("Tarea " + task + " eliminada");
-                    }
-                }
-            }
-            catch (Exception)
-            {
-            }
-        }
-
-        public static void ShowMenuAdd()
-        {
-            try
-            {
-                Console.WriteLine("Ingrese el nombre de la tarea: ");
-                string task = Console.ReadLine();
-                TL.Add(task);
-                Console.WriteLine("Tarea registrada");
-            }
-            catch (Exception)
-            {
-            }
-        }
-
-        public static void ShowMenuTres()
-        {
-            if (TL == null || TL.Count == 0)
-            {
-                Console.WriteLine("No hay tareas por realizar");
-            } 
-            else
-            {
-                Console.WriteLine("----------------------------------------");
-                for (int i = 0; i < TL.Count; i++)
-                {
-                    Console.WriteLine((i + 1) + ". " + TL[i]);
-                }
-                Console.WriteLine("----------------------------------------");
-            }
+            Console.WriteLine("El valor que ha ingresado es invalido");
+            ShowMenuRemove();
         }
     }
+    catch (Exception)
+    {
+        Console.WriteLine("Ha ocurrido un error al eliminar la tarea");
+    }
 }
+
+void ShowMenuAdd()
+{
+    try
+    {
+        Console.WriteLine("Ingrese el nombre de la tarea: ");
+        string nameTask = Console.ReadLine();
+        TaskList.Add(nameTask);
+        Console.WriteLine("Tarea registrada");
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Ha ocurrido un error al registrar la tarea");
+    }
+}
+
+void ShowMenuTaskList()
+{
+    if (TaskList?.Count > 0)
+    {
+        Console.WriteLine("----------------------------------------");
+        showTaskList();
+    }
+    else
+    {
+        Console.WriteLine("No hay tareas por realizar");
+    }
+}
+
+void showTaskList()
+{
+    var indexTask = 1;
+    TaskList.ForEach(p => Console.WriteLine($"{indexTask++} . {p}"));
+    Console.WriteLine("----------------------------------------");
+}
+
+
+public enum menuOptions
+{
+    Add = 1,
+    Remove = 2,
+    List = 3,
+    Exit = 4
+}
+
